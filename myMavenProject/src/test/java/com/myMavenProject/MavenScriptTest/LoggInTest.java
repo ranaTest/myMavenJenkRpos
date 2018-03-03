3094d4ff6675b0d91fdd3c951fe9cb8354f0bf32
rana <ranathakurace@hotmail.com>
2018-03-04 02:58:59 +0530

    3rd commit: adding chrome driver in test/resource

--- myMavenProject/src/test/java/com/myMavenProject/MavenScriptTest/LoggInTest.java
+++ myMavenProject/src/test/java/com/myMavenProject/MavenScriptTest/LoggInTest.java
@@ -1,5 +1,19 @@
 package com.myMavenProject.MavenScriptTest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class LoggInTest {
+	
+	
+	@Test
+	
+	public void loggin(){
+		
+	System.setProperty("webdriver.chrome.driver", "");
+	WebDriver driver=new ChromeDriver();
+	
+	}
 
 }
